ec8371ffcc3bf368a2f2e33d5330b3276222dd45
seba423 <kot152334@gmail.com>
2023-04-06 10:41:57 +0200

    not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-import "lib.py"
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
